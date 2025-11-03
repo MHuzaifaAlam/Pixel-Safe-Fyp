15904c5cd58cc551e361541321d58a5cd725be21
Ahmad <ahmad.malik12340@gmail.com>
2025-11-03 13:49:42 +0500

    login/signup

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -37,12 +37,20 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'users',
 ]
 
+INSTALLED_APPS += ['rest_framework.authtoken']
+
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  # 👈 must be at the top
+    'django.middleware.common.CommonMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -120,3 +128,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # 👈 React (Vite) dev server
+]
